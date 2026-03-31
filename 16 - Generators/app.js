ea5cd3854c6ccd8aa5033d163e6996dbb08aad29
Jalani <jalani2727@gmail.com>
2026-03-31 11:01:01 -0400

    add vite to generator folder - add project-wide package

--- 16 - Generators/app.js
+++ 16 - Generators/app.js
@@ -1,24 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Generators</title>
-</head>
-<body>
-<script>
-  
-  // The following will create a cascade of ajax requests.
-  
-  // The async library can handle creating cascades of ajax requests. This operation can also be done natively which is what this lesson aims to show using generators.
+const apiKey = import.meta.env.VITE_MARKETSTACK_KEY;
+console.log(apiKey)
+const marketStackURL = `http://api.marketstack.com/v1/eod?access_key=${apiKey}&symbols=AAPL`;
 
-  function ajax(url) {
-   fetch(url).then((response) => response.json()).then(responseJSON => dataGen.next(responseJSON))
-  }
+// The following will create a cascade of ajax requests.
 
-  function* steps() {
+// The async library can handle creating cascades of ajax requests. This operation can also be done natively which is what this lesson aims to show using generators.
+
+function ajax(url) {
+    fetch(url).then((response) => response.json()).then(responseJSON => dataGen.next(responseJSON))
+}
+
+function* steps() {
     console.log('fetching Market Data');
     const marketData = yield ajax(marketStackURL); // The result of the yield will be placed into your marketData variable
-    
+
     console.log(marketData);
 
     console.log('fetching jalani');
@@ -28,11 +23,7 @@
     console.log('fetching fat joe');
     const fatJoe = yield ajax('https://api.discogs.com/artists/51988'); // The result of the yield will be placed in your fatJoe variable.
     console.log(fatJoe);
-  }
+}
 
 const dataGen = steps();
 dataGen.next();
-
-</script>
-</body>
-</html>
