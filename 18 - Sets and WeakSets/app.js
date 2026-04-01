f46b8ee6a82dd4de865b90eec7b59ac68da0e5ef
Jalani <jalani2727@gmail.com>
2026-04-01 19:00:39 -0400

    and set up for remaining 3 modules in this walkthrough

--- 18 - Sets and WeakSets/app.js
+++ 18 - Sets and WeakSets/app.js
@@ -1,45 +1,61 @@
 // A set is similar to an array. It can be added to, removed from, and looped over.
-        // However, any particular item can only ever be added to it once (meaning it cannot contain duplicates) and it isn't index based. It's more of a list. mySet[0] would return undefined for example, not the first item in the list.
+// However, any particular item can only ever be added to it once (meaning it cannot contain duplicates) and it isn't index based. It's more of a list. mySet[0] would return undefined for example, not the first item in the list.
 
-        const people = new Set();
+const people = new Set();
 
-        people.add('Jalani');
-        people.add('Jose');
-        people.add('Ana');
+people.add('Jalani');
+people.add('Jose');
+people.add('Ana');
 
-        people.delete('Jalani'); // What's significant here is that the index doesn't need to be found to remove an item from a set. It also return true/false if an item is or is not deleted.
-
-
-        // Side-note* - .keys() will return the same thing as .values() when called on a Set.
-        console.log(people.keys());
-        // Additional side-note* - .entries() can be called on a set. Typically this will return a key/value pair but on a set it returns a value/value pair
-        console.log(people.entries());
-
-        // Also, a for-of loop will return ("yeild") each of the values - as you learned in the generators section of this walkthrough.
-        for (const person of people) {
-            console.log(person);
-        }
+people.delete('Jalani'); // What's significant here is that the index doesn't need to be found to remove an item from a set. It also return true/false if an item is or is not deleted.
 
 
-        // You can also pass an array of items directly to a Set when you are creating it and a Set will be created from the contents of the array.
-        const students = new Set(['Wes', 'Kara', 'Ana']);
+// Side-note* - .keys() will return the same thing as .values() when called on a Set.
+console.log(people.keys());
+// Additional side-note* - .entries() can be called on a set. Typically this will return a key/value pair but on a set it returns a value/value pair
+console.log(people.entries());
 
-        // .has() can be called on a set to check if a value is currently in the set.
-        console.log(students.has('Wes'))
-        console.log(students.has('Jalani'))
-        // .size will return the length of the set, similar to how .length will return the length
-        console.log(students.size)
+// Also, a for-of loop will return ("yeild") each of the values - as you learned in the generators section of this walkthrough.
+for (const person of people) {
+    console.log(person);
+}
 
-        // The values in a set can be accessed via people.values() and this returns a Set Iterator, which is functionally similar to a generator
-        // Calling .next() on .values() will yield the next value since a set iterator is returned.
-        
-        const rollCall = students.values();
-        
-        console.log(rollCall.next().value);
-        console.log(rollCall.next().value);
-        
-        // Imagine a student walks in! Add them to the set!
-        students.add('Leanne');
-        // Another!
-        students.add('Justin');
-        // Adding to the set will not change anything about rollCall Set Iterator. The set just gets more items for the Set Iterator to iterate through.
+
+// You can also pass an array of items directly to a Set when you are creating it and a Set will be created from the contents of the array.
+const students = new Set(['Wes', 'Kara', 'Ana']);
+
+// .has() can be called on a set to check if a value is currently in the set.
+console.log(students.has('Wes'))
+console.log(students.has('Jalani'))
+// .size will return the length of the set, similar to how .length will return the length
+console.log(students.size)
+
+// The values in a set can be accessed via people.values() and this returns a Set Iterator, which is functionally similar to a generator
+// Calling .next() on .values() will yield the next value since a set iterator is returned.
+
+const rollCall = students.values();
+
+console.log(rollCall.next().value);
+console.log(rollCall.next().value);
+
+// Imagine a student walks in! Add them to the set!
+students.add('Leanne');
+// Another!
+students.add('Justin');
+// Adding to the set will not change anything about rollCall Set Iterator. The set just gets more items for the Set Iterator to iterate through.
+
+
+
+// Weak Sets - A Weak Set can only every contain Objects. No strings, arrays, numbers, etc.
+
+let dog1 = { name: 'Snickers', age: 3 };
+let dog2 = { name: 'sunny', age: 1 };
+
+const weakSauce = new WeakSet([dog1, dog2])
+
+// They can't be iterated upon. For-of loops will return an error if used on Weak Sets.
+
+// Weak Sets don't have a .clear() method. If one of the objects inside of your weak set is deleted, it's also deleted from the set.
+console.log(weakSauce);
+dog1 = null;
+console.log(weakSauce);
